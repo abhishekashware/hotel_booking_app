fd00ae02a82b80cdf3413de9a3a40cb59edcc5bc
Ashware <abhishekashware12517@gmail.com>
2023-03-12 18:43:30 +0530

    changes

--- api/client/src/index.js
+++ api/client/src/index.js
@@ -5,7 +5,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://127.0.0.1:8800/api/';
+axios.defaults.baseURL = 'https://hotel-booking-app-ndut.onrender.com/api/';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
